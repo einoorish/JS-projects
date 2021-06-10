39fc31ce6086fbf05b2047b16bd2ab3d1e4597cc
E. H <35835232+einoorish@users.noreply.github.com>
2021-06-11 00:26:01 +0300

    ui +marked

---
+++ notes_app/script.js
@@ -0,0 +1,16 @@
+const editBtn = document.querySelector(".edit");
+const deleteBtn = document.querySelector(".delete");
+
+const main = document.querySelector(".main");
+const textArea = document.querySelector("textarea");
+
+editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+});
+
+textArea.addEventListener("input", (e) => {
+  const { value } = e.target;
+
+  main.innerHTML = marked(value);
+});
